719763628c06907b04c157572bca6f2b99e19eb4
Satoshi Imai <satoshi.imai@gmail.com>
2019-01-04 17:36:58 +0900

    Add debug-push-reader

--- src/cl-debug-print.lisp
+++ src/cl-debug-print.lisp
@@ -1,7 +1,15 @@
 (defpackage cl-debug-print
   (:nicknames :debug-print)
-  (:use :cl :cl-syntax :named-readtables)
-  (:export :debug-print :debug-print-reader :debug-print-syntax))
+  (:use #:cl
+        #:cl-syntax
+        #:named-readtables)
+  (:export #:debug-print
+           #:debug-print-reader
+           #:debug-push
+           #:debug-push-reader
+           #:*dbg*
+           #:init-dbg
+           #:debug-print-syntax))
 (in-package :cl-debug-print)
 
 (defun debug-print (pre-exp exp)
@@ -13,6 +21,21 @@
   (let ((read-data (read stream t nil t)))
     `(debug-print (quote ,read-data) ,read-data)))
 
+(defvar *dbg* nil)
+
+(defun init-dbg ()
+  (setf *dbg* nil))
+
+(defun debug-push (obj)
+  (push obj cl-debug-print:*dbg*)
+  obj)
+
+(defun debug-push-reader (stream char1 char2)
+  (declare (ignore char1 char2))
+  (let ((read-data (read stream t nil t)))
+    `(debug-push ,read-data)))
+
 (defsyntax debug-print-syntax
   (:merge :standard)
-  (:dispatch-macro-char #\# #\> #'debug-print-reader))
+  (:dispatch-macro-char #\# #\> #'debug-print-reader)
+  (:dispatch-macro-char #\# #\! #'debug-push-reader))
