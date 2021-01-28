5f6b0b359aed47b01b7c97d2a486d42c70629656
Satoshi Imai <satoshi.imai@gmail.com>
2021-01-29 07:05:14 +0900

    Add variables for describe mode / destination setting

--- src/cl-debug-print.lisp
+++ src/cl-debug-print.lisp
@@ -7,21 +7,35 @@
            #:debug-print-reader
            #:debug-push
            #:debug-push-reader
+           #:*use-describe*
+           #:*destination*
            #:*dbg*
            #:clear-dbg
            #:debug-print-syntax))
 (in-package :cl-debug-print)
 
+(defvar *use-describe* nil
+  "When *USE-DESCRIBE* is true, describe is used for debug prints.")
+
+(defvar *destination* *standard-output*
+  "*DESTINATION* allows to configure destination stream for debug prints.")
+
+(defvar *dbg* nil
+  "*DBG* is a list, and using DEBUG-PUSH(#!) will be pushed to this list.
+To clear this variable, use the CLEAR-DBG function.")
+
 (defun debug-print (pre-exp exp)
-  (format *standard-output* "~S => ~S~%" pre-exp exp)
+  (if *use-describe*
+      (format *destination* "~S => ~A~%" pre-exp
+              (with-output-to-string (s)
+                (describe exp s)))
+      (format *destination* "~S => ~S~%" pre-exp exp))
   exp)
 
 (defun debug-print-reader (stream char1 char2)
   (declare (ignore char1 char2))
   (let ((read-data (read stream t nil t)))
     `(debug-print (quote ,read-data) ,read-data)))
-
-(defvar *dbg* nil)
 
 (defun clear-dbg ()
   (setf *dbg* nil))
